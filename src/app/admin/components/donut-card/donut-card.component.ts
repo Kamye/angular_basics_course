370d6c7f30147857b1a5de07fe472e4a1f23720d
Андрей Ганжа <andrej15ganzha@gmail.com>
2025-05-17 23:19:11 +0300

    Vew Encapsulation: Shadow DOM and Emulated

--- src/app/admin/components/donut-card/donut-card.component.ts
+++ src/app/admin/components/donut-card/donut-card.component.ts
@@ -1,9 +1,14 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  Input,
+  ViewEncapsulation
+} from '@angular/core';
 
 import { Donut } from "../../models/donut.model";
 
 @Component({
   selector: 'app-donut-card',
+  encapsulation: ViewEncapsulation.Emulated,
   template: `
     <img
       src="/assets/img/{{ donut.icon }}.svg"
