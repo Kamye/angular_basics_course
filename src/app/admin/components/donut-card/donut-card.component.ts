9a5828d4e4fb2e4e1c763067a7c9835696dce6ba
Андрей Ганжа <andrej15ganzha@gmail.com>
2025-05-17 23:04:00 +0300

    scoped styles in components

--- src/app/admin/components/donut-card/donut-card.component.ts
+++ src/app/admin/components/donut-card/donut-card.component.ts
@@ -12,16 +12,41 @@ import { Donut } from "../../models/donut.model";
         class="donut-card-icon"
       />
       <div>
-        <p class="donat-card-name">
+        <p class="donut-card-name">
           {{ donut.name }}
         </p>
-        <p class="donat-card-price">
+        <p class="donut-card-price">
           {{ donut.price }}
         </p>
       </div>
     </div>
   `,
   styles: [
+    `
+      .donut-card {
+        display: flex;
+        align-items: center;
+        background: #f7f7f7;
+        border-radius: 5px;
+        margin-bottom: 5px;
+        padding: 5px 15px;
+        transition: transform 0.2s ease-in-out;
+        &:hover {
+          transform: translateY(-3px);
+        }
+        &-name {
+          font-size: 16px;
+        }
+        &-price {
+          font-size: 14px;
+          color: #c14583;
+        }
+        &-icon {
+          width: 50px;
+          margin-right: 10px;
+        }
+      }
+    `
   ]
 })
 export class DonutCardComponent {
